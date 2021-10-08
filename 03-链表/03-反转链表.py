66a7bdea3ee602b410dbd9d3800baf251c628f8a
hanjie <hanjie1225@126.com>
2021-10-08 14:33:41 +0800

    Twelfth commit

---
+++ 03-链表/03-反转链表.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : OrangeCH3
+# @Time     : 2021.10.07 14:39
+# @File     : 03-反转链表.py
+# @Project  : AGTD
+
+
+# 代码在力扣网页端可编译通过
+
+
+# 反转一个单链表。
+
+
+# 1. 双指针
+def reverseLinkedList(head):
+
+    cur = head
+    pre = None
+
+    while cur != None:
+        tmp = cur.next
+
+        cur.next = pre
+
+        # 更新pre、cur指针
+        pre = cur
+        cur = tmp
+
+    return pre
+
+
+# 2. 递归法
+def reverseLinkedList2(head):
+
+    def reverse(pre, cur):
+        if not cur:
+            return pre
+
+        tmp = cur.next
+        cur.next = pre
+
+        return reverse(cur, tmp)
+
+    return reverse(None, head)

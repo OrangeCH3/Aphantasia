ead9a4987fc77db682ee76ff6a760458cd123176
hanjie <hanjie1225@126.com>
2021-10-22 08:21:35 +0800

    Fifty-sixth commit

---
+++ 07-二叉树/17-最大二叉树.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : OrangeCH3
+# @Time     : 2021.10.20 10:34
+# @File     : 17-最大二叉树.py
+# @Project  : AGTD
+
+
+# 给定一个不含重复元素的整数数组。一个以此数组构建的最大二叉树定义如下：
+# 1. 二叉树的根是数组中的最大元素。
+# 2. 左子树是通过数组中最大值左边部分构造出的最大二叉树。
+# 3. 右子树是通过数组中最大值右边部分构造出的最大二叉树。
+# 通过给定的数组构建最大二叉树，并且输出这个树的根节点。
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    def constructMaximumBinaryTree(self, nums):
+
+        if not nums:
+            return None
+
+        root = TreeNode(max(nums))
+
+        cut_idx = nums.index(root.val)
+
+        if cut_idx > 0:
+            root.left = self.constructMaximumBinaryTree(nums[:cut_idx])
+
+        if p > len(nums):
+            root.right = self.constructMaximumBinaryTree(nums[p+1:])
+
+        return root

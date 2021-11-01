f91d4488d46f03e94a06b878041cb8d2c19a8b9b
hanjie <hanjie1225@126.com>
2021-11-01 09:34:59 +0800

    Eighty-eighth commit

--- 09-贪心算法/01-分发饼干.py
+++ 09-贪心算法/01-分发饼干.py
@@ -6,7 +6,25 @@
 # @Project  : AGTD
 
 
+class Solution(object):
+    # 可以使用贪心策略，先将饼干数组和小孩数组排序。
+    # 然后从后向前遍历小孩数组，用大饼干优先满足胃口大的，并统计满足小孩数量。
+    def findContentChildren(self, g, s):
+
+        g.sort()
+        s.sort()
+        start, count = len(s)-1, 0
+
+        for index in range(len(g)-1, -1, -1):
+            if start >= 0 and g[index] <= s[start]:
+                start -= 1
+                count += 1
+        return count
 
 
-
-
+if __name__ == '__main__':
+    g = [1, 2, 7, 10]
+    s = [1, 3, 5, 9]
+    solution = Solution()
+    res = solution.findContentChildren(g, s)
+    print(res)

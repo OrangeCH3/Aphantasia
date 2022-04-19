542749182f34264be44d34ca9453d8839f4327bd
hanjie <hanjie1225@126.com>
2022-04-19 11:12:42 +0800

    OneHundred-96th commit

--- 04-哈希表/07-三数之和.py
+++ 04-哈希表/07-三数之和.py
@@ -7,7 +7,7 @@
 
 
 # 给你一个包含 n 个整数的数组 nums，
-# 判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+# 判断 nums 中是否存在三个元素 a，b，c，使得 a + b + c = 0 ？
 # 请你找出所有满足条件且不重复的三元组。
 # 注意：答案中不可以包含重复的三元组。
 
@@ -29,7 +29,7 @@ class Solution(object):
             if nums[i] > 0:
                 break
 
-            if i >= 1 and nums[i] == nums[i-1]:
+            if i >= 1 and nums[i] == nums[i - 1]:
                 continue
 
             while left < right:
@@ -40,8 +40,8 @@ class Solution(object):
                     left += 1
                 else:
                     ans.append([nums[i], nums[left], nums[right]])
-                    while left != right and nums[left] == nums[left+1]: left += 1
-                    while left != right and nums[right] == nums[right-1]: right -= 1
+                    while left != right and nums[left] == nums[left + 1]: left += 1
+                    while left != right and nums[right] == nums[right - 1]: right -= 1
                     left += 1
                     right -= 1
 
@@ -49,7 +49,6 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-
     nums = [-1, 0, 1, 2, -1, -4, 7, -7, 6, -6]
 
     s = Solution()

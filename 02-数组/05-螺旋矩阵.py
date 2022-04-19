d659696a0c9c9511beb6f022399435bf6146a449
hanjie <hanjie1225@126.com>
2022-04-19 10:00:57 +0800

    OneHundred-91th commit

--- 02-数组/05-螺旋矩阵.py
+++ 02-数组/05-螺旋矩阵.py
@@ -7,7 +7,7 @@
 
 
 # 给定一个正整数 n
-# 生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵
+# 生成一个包含 1 到 n² 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵
 
 def spiral_matrix(n):
 
@@ -33,7 +33,7 @@ def spiral_matrix(n):
 
         # 从右到左填充下边
         for z in range(right, left, -1):
-            matrix[down][x] = number
+            matrix[down][z] = number
             number += 1
 
         # 从下到上填充左边

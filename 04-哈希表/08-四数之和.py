9678ebc0fb5fcd7eb55d4baa4b9881766bb99138
hanjie <hanjie1225@126.com>
2022-04-19 11:13:15 +0800

    OneHundred-97th commit

--- 04-哈希表/08-四数之和.py
+++ 04-哈希表/08-四数之和.py
@@ -23,22 +23,24 @@ class Solution(object):
 
         for i in range(n):
 
-            if i > 0 and nums[i] == nums[i-1]: continue
+            if i > 0 and nums[i] == nums[i - 1]: continue
 
-            for j in range(i+1, n):
+            for j in range(i + 1, n):
 
-                if j > i+1 and nums[j] == nums[j-1]: continue
+                if j > i + 1 and nums[j] == nums[j - 1]: continue
 
                 p = j + 1
                 q = n - 1
 
                 while p < q:
-                    if nums[i] + nums[j] + nums[p] + nums[q] > target: q -= 1
-                    elif nums[i] + nums[j] + nums[p] + nums[q] < target: p += 1
+                    if nums[i] + nums[j] + nums[p] + nums[q] > target:
+                        q -= 1
+                    elif nums[i] + nums[j] + nums[p] + nums[q] < target:
+                        p += 1
                     else:
                         res.append([nums[i], nums[j], nums[p], nums[q]])
-                        while p < q and nums[p] == nums[p+1]: p += 1
-                        while p < q and nums[q] == nums[q-1]: q -= 1
+                        while p < q and nums[p] == nums[p + 1]: p += 1
+                        while p < q and nums[q] == nums[q - 1]: q -= 1
                         p += 1
                         q -= 1
 
@@ -46,7 +48,6 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-
     nums = [1, 0, -1, 0, -2, 2, 6, -7, 3, 4, -5]
     target = 2
 

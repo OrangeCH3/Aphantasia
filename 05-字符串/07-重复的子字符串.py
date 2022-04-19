438e44904581cc94dbc3af3bfeb875615a65eecc
hanjie <hanjie1225@126.com>
2022-04-19 14:16:42 +0800

    TwoHundred-first commit

--- 05-字符串/07-重复的子字符串.py
+++ 05-字符串/07-重复的子字符串.py
@@ -21,18 +21,17 @@ class Solution(object):
         self.getNext(next, s)
         print(next)
 
-        if next[-1] != 0 and len(s) % (len(s)-next[-1]) == 0:
+        if next[-1] != 0 and len(s) % (len(s) - next[-1]) == 0:
             return True
 
         return False
-
 
     def getNext(self, nxt, s):
         nxt[0] = 0
         j = 0
         for i in range(1, len(s)):
             while j > 0 and s[i] != s[j]:
-                j = nxt[j-1]
+                j = nxt[j - 1]
             if s[i] == s[j]:
                 j += 1
             nxt[i] = j
@@ -40,7 +39,6 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-
     str1 = "abcabcabcabc"
     s = Solution()
     res = s.repeatedSubstringPattern(str1)

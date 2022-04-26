b4ce374998db551f6d229d04e984a5722042779c
hanjie <hanjie1225@126.com>
2022-04-26 09:30:31 +0800

    TwoHundred-37th commit

--- 07-二叉树/10-二叉树的所有路径.py
+++ 07-二叉树/10-二叉树的所有路径.py
@@ -9,23 +9,26 @@
 class Solution(object):
 
     def binaryTreePaths(self, root):
-        # initpath = []
         res = []
+        path = []
 
-        def backtrace(node, path=[]):
-            # if not node:
-            #     return
-            path.append(node.val)
+        def backtrace(node):
+
+            path.append(str(node.val))
+
             if not node.left and not node.right:
                 res.append(path[:])
                 return
 
             ways = []
+
             if node.left: ways.append(node.left)
             if node.right: ways.append(node.right)
+
             for way in ways:
-                backtrace(way, path)
-                path.pop()  # 回溯
+                backtrace(way)
+                path.pop()
 
         backtrace(root)
-        return ['->'.join(list(map(str, i))) for i in res]
+
+        return ['->'.join(val) for val in res]

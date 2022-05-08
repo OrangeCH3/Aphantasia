9e3f6a8af6ddff62f97971cd643f3f45165960d1
hanjie <hanjie1225@126.com>
2022-05-08 10:00:43 +0800

    TwoHundred-67th commit

--- 11-单调栈/02-下一个更大元素.py
+++ 11-单调栈/02-下一个更大元素.py
@@ -9,22 +9,22 @@
 class Solution(object):
 
     def nextGreaterElement(self, nums1, nums2):
-        n = len(nums1)
-        result = [-1] * n
+
+        res = [-1] * len(nums1)
         stack = [0]
 
-        for i in range(1, n):
+        for i in range(1, len(nums2)):
             if nums2[i] <= nums2[stack[-1]]:
                 stack.append(i)
             else:
                 while len(stack) != 0 and nums2[i] > nums2[stack[-1]]:
                     if nums2[stack[-1]] in nums1:
                         index = nums1.index(nums2[stack[-1]])
-                        result[index] = nums2[i]
+                        res[index] = nums2[i]
                     stack.pop()
                 stack.append(i)
 
-        return result
+        return res
 
 
 if __name__ == '__main__':

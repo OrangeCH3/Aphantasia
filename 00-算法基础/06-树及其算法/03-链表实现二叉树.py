22f3ec85e72b4a9e79a1cb973cb6f85d13164d80
hanjie <hanjie1225@126.com>
2022-05-17 16:35:26 +0800

    TwoHundred-78th commit

---
+++ 00-算法基础/06-树及其算法/03-链表实现二叉树.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : OrangeCH3
+# @Time     : 2022.04.18 10:24
+# @File     : 03-链表实现二叉树.py
+# @Project  : AGTD
+
+
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+    def insertLeft(self, newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+
+
+if __name__ == '__main__':
+    r = BinaryTree('a')
+    print(r.getRootVal())
+    print(r.getLeftChild())
+    r.insertLeft('b')
+    print(r.getLeftChild())
+    print(r.getLeftChild().getRootVal())
+    r.insertRight('c')
+    print(r.getRightChild())
+    print(r.getRightChild().getRootVal())
+    r.getRightChild().setRootVal('hello')
+    print(r.getRightChild().getRootVal())
